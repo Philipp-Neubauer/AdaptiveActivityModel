6f212c3057ded7d3c8bc19aa7203998fe9969796
Philipp-Neubauer <neubauer.phil@gmail.com>
2017-03-10 16:21:12 +1300

    dockerized

--- draft/run.sh
+++ draft/run.sh
@@ -4,5 +4,7 @@ set -ex
 
 docker build -t docker.dragonfly.co.nz/adaptive_activity .
 
+docker push docker.dragonfly.co.nz/adaptive_activity
+
 docker run --rm  -v $PWD:/work -w /work \
   docker.dragonfly.co.nz/adaptive_activity ./build.sh
